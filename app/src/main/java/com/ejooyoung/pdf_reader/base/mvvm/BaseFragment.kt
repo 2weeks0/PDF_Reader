b559597d7cbd0a3de3928ad47e0574b704d7f03a
fs-2weeks0 <2weeks0@fasoo.com>
2020-12-16 09:05:34 +0900

    feat (BaseAndroidViewModel  / BaseFragment) : 생명주기와 disposable을 관리 및 연결해주는 Base class 생성
    * ViewModel을 갖고 있는 Fragment는 BaseFragment를 상속받아야 하고,
    ViewModel은 BaseAndroidViewModel을 상속받아야 함

---
+++ app/src/main/java/com/ejooyoung/pdf_reader/base/mvvm/BaseFragment.kt
@@ -0,0 +1,44 @@
+package com.ejooyoung.pdf_reader.base.mvvm
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<A: BaseAndroidViewModel, B: ViewDataBinding> : Fragment() {
+
+    protected lateinit var binding: B
+    lateinit var viewModel: A
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setupViewModel()
+        setupDataBinding(inflater, container)
+        setupObserver()
+        onBindingCreated()
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewModel.onPause()
+    }
+
+    protected abstract fun setupViewModel()
+
+    protected abstract fun setupDataBinding(inflater: LayoutInflater, container: ViewGroup?)
+
+    protected abstract fun setupObserver()
+
+    protected abstract fun onBindingCreated()
+}
